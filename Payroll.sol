2ab4555a9960804376428a0bd7d5eb09461c92fb
Dafei Ning <ningdafei@gmail.com>
2018-10-18 18:19:10 -0700

    update functions

--- Payroll.sol
+++ Payroll.sol
@@ -1,28 +1,34 @@
 pragma solidity ^0.4.25;
 
 contract Payroll {
+	address          owner;
+	
 	uint             salary       = 1 ether;
     address          employee     = 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
     uint    constant payDuration  = 30 days;
     uint             lastPayday   = now;
 
     function Payroll() {
-        
+        owner = msg.sender;
     }
 
-    function addFund() returns(uint){
+    function addFund() payable returns(uint){
         return this.balance;
     }
 
-    function calculateRunway() returns (uint) {
-        
+    function calculateRunway() payable returns (uint) {
+        return this.balance / salary;
     } 
 
     function hasEnoughFund() returns (bool) {
-        
+        return calculateRunway() > 0;
     }
 
     function getPaid() {
-        
+        if (lastPayday + payDuration > now) {
+            revert();  // reverse the state and return the gas
+        }
+        lastPayday = lastPayday + payDuration; // the internal status change must come before external transfer
+        employee.transfer(salary);
     }
 }

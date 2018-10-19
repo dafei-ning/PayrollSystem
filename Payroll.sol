8467242784cc18ca8a44e187f242d1392e1ffc14
Dafei Ning <ningdafei@gmail.com>
2018-10-18 18:07:07 -0700

    intiate functions

--- Payroll.sol
+++ Payroll.sol
@@ -1,2 +1,28 @@
 pragma solidity ^0.4.25;
 
+contract Payroll {
+	uint             salary       = 1 ether;
+    address          employee     = 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
+    uint    constant payDuration  = 30 days;
+    uint             lastPayday   = now;
+
+    function Payroll() {
+        
+    }
+
+    function addFund() returns(uint){
+        return this.balance;
+    }
+
+    function calculateRunway() returns (uint) {
+        
+    } 
+
+    function hasEnoughFund() returns (bool) {
+        
+    }
+
+    function getPaid() {
+        
+    }
+}

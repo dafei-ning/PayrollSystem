a34eb6ed87f125dc65bf582ad179b2d1c087637e
Dafei Ning <ningdafei@gmail.com>
2018-11-17 01:32:25 -0800

    add helper functions and update add&removeEmployee

--- Payroll.sol
+++ Payroll.sol
@@ -18,25 +18,48 @@ contract Payroll {
         owner = msg.sender;
     }
 
-    function addEmployee(address employee, uint salary) {
-        require(msg.sender == owner);
+    function _partialPaid(Employee employee) {
+        uint payment = employee.salary * (now - employee.lastPayday) / payDuration;
+        employee.id.transfer(payment);
+    }
+
+    function _findEmployee(address employeeID) returns (Employee) {
         for (uint i = 0; i < employees.length; i++) {
-            if (employees[i].id == employee) {
-                revert("The employee address exists in database!")
+            if (employees[i].id == employeeID) {
+                return employees[i];
             }
         }
+    }
+
+    function addEmployee(address employeeID, uint salary) {
+        require(msg.sender == owner);
+        Employee employee = _findEmployee(employeeID);
         employees.push(Employee(employee, salary, now));
     }
 
-    function updateEmployee(address e, uint s) {
+    function removeEmployee(address employeeID) {
         require(msg.sender == owner);
-        if (employee != 0x0) {
-            uint payment = salary * (now - lastPayday) / payDuration;
-            employee.transfer(payment);
+        for (uint i = 0; i < employees.length; i++) {
+            if (employees[i].id == employee) {
+                _partialPaid(employees[i]);
+                delete employees[i];  // delete以后位置是空白的，不太便于日后操作
+                employees[i] = employees[employees.length - 1]; // 一个技巧是将最后一个employee提到被删的那个位置上
+                employees.length -= 1; // 然后将数组size缩小
+                return;
+            }
         }
-        employee = e;
-        salary = s * 1 ether;
-        lastPayday = now;
+    }
+
+    function updateEmployee(address employeeID, uint salary) {
+        require(msg.sender == owner);
+        for (uint i = 0; i < employees.length; i++) {
+            if (employees[i].id == employeeID) {
+                _partialPaid(employees[i]);
+                employees[i].salary = salary;
+                employees[i].lastPayday = now;
+                return;
+            }
+        }
     }
 
     
